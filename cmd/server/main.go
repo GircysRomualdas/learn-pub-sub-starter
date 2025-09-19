855dc10f7313e353c0467174ec87b5d80c7b324b
romas <r2udis@gmail.com>
2025-09-20 00:53:10 +0300

    add pubsub publish json

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,9 @@ import (
 	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
@@ -18,6 +21,17 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Connected to RabbitMQ")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Failed to open a channel:", err)
+		return
+	}
+	defer ch.Close()
+
+	if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true}); err != nil {
+		fmt.Println("publish error:", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
