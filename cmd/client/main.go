c5a8af7594166fb41c09a8fe60a34a6b76aa5c66
romas <r2udis@gmail.com>
2025-09-26 22:30:40 +0300

    add client commands

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 
@@ -16,7 +14,7 @@ func main() {
 	connStr := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
-		fmt.Println("Failed to connect to RabbitMQ:", err)
+		fmt.Printf("Failed to connect to RabbitMQ: %v\n", err)
 		return
 	}
 	defer conn.Close()
@@ -25,22 +23,53 @@ func main() {
 
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
-		fmt.Println("Failed to welcome client:", err)
+		fmt.Printf("Failed to welcome client: %v\n", err)
 		return
 	}
 
 	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
 	ch, _, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.QueueTransient)
 	if err != nil {
-		fmt.Println("Failed to declare and bind queue:", err)
+		fmt.Printf("Failed to declare and bind queue: %v\n", err)
 		return
 	}
 	defer ch.Close()
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	gameState := gamelogic.NewGameState(username)
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
 
-	fmt.Println("Exiting...")
+		switch input[0] {
+		case "spawn":
+			err := gameState.CommandSpawn(input)
+			if err != nil {
+				fmt.Printf("Spawn error: %v\n", err)
+			}
+		case "move":
+			armyMove, err := gameState.CommandMove(input)
+			if err != nil {
+				fmt.Printf("Move error: %v\n", err)
+				continue
+			}
+			if len(armyMove.Units) == 0 {
+				fmt.Printf("No units to move\n")
+				continue
+			}
+		case "status":
+			gameState.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Printf("Unknown command: %s\n", input[0])
+		}
+	}
 
 }
